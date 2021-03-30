6dc2bd9f45ccd1c9d33f4234b3b4f71400c03a0a
slava <slava.komlev@gmail.com>
2021-03-30 22:12:36 +1300

    Hello world

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,7 +10,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine('Hello world!');
+            Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
     }
