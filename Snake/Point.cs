492631a6ed9f33ea68875a412f3df6b842c4045f
slava <slava.komlev@gmail.com>
2021-04-06 18:17:28 +1200

    class Point

---
+++ Snake/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symb;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write(symb);
+        }
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,8 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine('Hello world!');
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symb = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.symb = '#';
+            p2.Draw();
+
             Console.ReadLine();
+
         }
     }
 }
