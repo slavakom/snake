028697df2fb2e022c1a757d79a68bed16e4f105b
slava <slava.komlev@gmail.com>
2021-04-06 21:18:17 +1200

    Lesson5_10

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -10,13 +11,38 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
+           /* Point p1 = new Point(1,3,'*');
             p1.Draw();
 
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
-            Console.ReadLine();
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            line.Draw();
+
+            VerticalLine vline = new VerticalLine(5, 5, 10, '+');
+            vline.Draw();
+           */
+           Console.SetBufferSize(80,25);
+           HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
+           HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+           VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+           VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
+           upLine.Draw();
+           downLine.Draw();
+           leftLine.Draw();
+           rightLine.Draw();
+
+           
+           //Drawing
+           Point p = new Point(4,5,'*');
+           Snake snake = new Snake(p,4,Direction.RIGHT);
+           snake.Draw();
+           for (int i = 0; i < 10; i++)
+           {
+               snake.Move();
+               Thread.Sleep(300);
+           }
 
         }
     }
---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int x, int yStart,int yEnd,char symb)
+        {
+            pList = new List<Point>();
+            for (int y = yStart; y <= yEnd; y++)
+            {
+                Point p = new Point(x, y, symb);
+                pList.Add(p);
+            }
+        }
+
+        
+    }
+}
